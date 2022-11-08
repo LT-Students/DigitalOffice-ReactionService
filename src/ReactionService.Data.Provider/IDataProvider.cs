0907fa49d5b99ab241de073e2829a12d174d10ff
maybee16 <mayaochir@gmail.com>
2022-11-08 17:32:05 +0900

    Added empty service

---
+++ src/ReactionService.Data.Provider/IDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.EFSupport.Provider;
+using LT.DigitalOffice.Kernel.Enums;
+
+namespace LT.DigitalOffice.ReactionService.Data.Provider;
+
+[AutoInject(InjectType.Scoped)]
+public interface IDataProvider : IBaseDataProvider
+{
+}
---
+++ src/ReactionService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -0,0 +1,11 @@
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.Attributes;
+using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+using LT.DigitalOffice.Models.Broker.Requests.User;
+
+namespace LT.DigitalOffice.ReactionService.Models.Dto.Configurations;
+
+public class RabbitMqConfig : BaseRabbitMqConfig
+{
+  [AutoInjectRequest(typeof(IGetUsersDataRequest))]
+  public string GetUsersDataEndpoint { get; set; }
+}
