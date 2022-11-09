c4304d225d93b05645deeb4a26560b8195f75574
maybee16 <mayaochir@gmail.com>
2022-11-09 23:00:57 +0900

    Fixes

--- src/ReactionService.Models.Dto/Configurations/RabbitMqConfig.cs
+++ src/ReactionService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -7,7 +7,6 @@ namespace LT.DigitalOffice.ReactionService.Models.Dto.Configurations;
 public class RabbitMqConfig : BaseRabbitMqConfig
 {
   // Image
-
   [AutoInjectRequest(typeof(IGetImagesRequest))]
   public string GetImagesEndpoint { get; set; }
 
