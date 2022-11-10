39b5fb7bb791aa7f597ae8c0cf78802891142d18
Maya O <55130636+maybee16@users.noreply.github.com>
2022-11-10 20:28:13 +0900

    Added empty service (#1)
    * Added empty service
    
    * Fixes
    
    * Fixes
    
    * Update .editorconfig
    
    Co-authored-by: Igor Likhachev <Fdiii@mail.ru>
    
    * Fixes
    
    Co-authored-by: Igor Likhachev <Fdiii@mail.ru>

---
+++ src/ReactionService.Data.Provider.MsSql.Ef/ReactionServiceDbContext.cs
@@ -0,0 +1,46 @@
+﻿using LT.DigitalOffice.Kernel.EFSupport.Provider;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.ReactionService.Data.Provider.MsSql.Ef;
+
+public class ReactionServiceDbContext : DbContext, IDataProvider
+{
+  public ReactionServiceDbContext(DbContextOptions<ReactionServiceDbContext> options)
+    : base(options)
+  {
+  }
+
+  // Fluent API is written here.
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.ApplyConfigurationsFromAssembly(Assembly.Load("LT.DigitalOffice.ReactionService.Models.Db"));
+  }
+
+  public object MakeEntityDetached(object obj)
+  {
+    Entry(obj).State = EntityState.Detached;
+    return Entry(obj).State;
+  }
+
+  async Task IBaseDataProvider.SaveAsync()
+  {
+    await SaveChangesAsync();
+  }
+
+  void IBaseDataProvider.Save()
+  {
+    SaveChanges();
+  }
+
+  public void EnsureDeleted()
+  {
+    Database.EnsureDeleted();
+  }
+
+  public bool IsInMemory()
+  {
+    return Database.IsInMemory();
+  }
+}
---
+++ src/ReactionService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -0,0 +1,16 @@
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.Attributes;
+using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+using LT.DigitalOffice.Models.Broker.Requests.Image;
+
+namespace LT.DigitalOffice.ReactionService.Models.Dto.Configurations;
+
+public class RabbitMqConfig : BaseRabbitMqConfig
+{
+  // Image
+
+  [AutoInjectRequest(typeof(IGetImagesRequest))]
+  public string GetImagesEndpoint { get; set; }
+
+  [AutoInjectRequest(typeof(ICreateImagesRequest))]
+  public string CreateImagesEndpoint { get; set; }
+}
